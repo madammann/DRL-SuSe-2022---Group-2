a79ab8b6b9b15502f00b9c8e5e39e92fe2be4224
Marlon Dammann <64379599+madammann@users.noreply.github.com>
2022-04-30 18:24:14 +0200

    pushed changes from offline session

--- Homework 2/gridworld.py
+++ Homework 2/gridworld.py
@@ -18,24 +18,18 @@ class Tile:
             chance = random.random()
             if self.transition_prob[action][0] >= chance:
                 return (0,1) # go up
-            elif self.transition_prob[action][1] >= chance:
+            elif np.sum(self.transition_prob[action][:1]) >= chance:
                 return (1,0) # go right
-            elif self.transition_prob[action][2] >= chance:
+            elif np.sum(self.transition_prob[action][:2]) >= chance:
                 return (0,-1) # go down
-            elif self.transition_prob[action][3] >= chance:
+            elif np.sum(self.transition_prob[action][:3]) >= chance:
                 return (-1,0) # go left
 
-    def get_possible(self, choice):
-        '''Returns True if a choice (0-3 int) is possible on this tile.'''
-        return self.options[choice]
-
 class Gridworld:
-    agent_starting_location = (0,0)
-
     def __init__(self, size = 5, seed = 3):
 
         self.size = size
-
+        self.agent_location = (0,0)
         random.seed(seed)
         np.random.seed(seed)
 
@@ -46,7 +40,7 @@ class Gridworld:
 
         #0:free space, 1:wall, 2:target, 3:toxic, 4:wind (, 9:agent)
         self.grid = np.asarray(random.choices(population = (0,1,3,4), weights = rnd_weights, k = size**2)).reshape(size, size)
-        self.grid[Gridworld.agent_starting_location] = 0
+        self.grid[self.agent_location] = 0
 
         #set target location
         self.grid[np.random.randint(size/2,size), np.random.randint(size/2,size)] = 2
@@ -105,10 +99,14 @@ class Gridworld:
                     self.tiles[x][y].transition_prob = wind_prob
 
     def reset(self):
-        pass
+        self.location = (0,0)
 
-    def step(self):
-        pass
+    def step(self, action):
+        self.agent_location += self.get_tile_at(self.agent_location).move(action)
+        terminal = self.grid[self.agent_location] == 2
+        return self.agent_location, self.get_tile_at(self.agent_location).reward, terminal
+
+        return state, reward, terminal
 
     def visualize(self, agent):
 
@@ -132,46 +130,80 @@ class Gridworld:
     def get_tile_at(self, coordinates):
         return self.tiles[coordinates[0],coordinates[1]]
 
-    def prove_solvable(self, location=(0,0)):
-        #Todo: avoid forever-loop, maybe include tile-checked-flag
-        neighbors = [(location[0]+1,location[1]),(location[0]-1,location[1]),(location[0],location[1]+1),(location[0],location[1]-1)]
-        if any([self.get_tile_at(coord).reward > 0 for coord in neighbors]):
-            return True # returns true if the positive reward is in the neighborhood
-        else:
-            for neighbor in neighbors:
-                if not self.get_tile_at(neighbor).obstructed:
-                    self.prove_solvable(location=neighbor)
-            return False # returns false if running out of neighbor options
+    def update_q_value(self,location, action, value):
+        pass
+
+    # def prove_solvable(self, location=(0,0), visited=[]):
+    #     #Todo: avoid forever-loop, maybe include tile-checked-flag
+    #     neighbors = [(location[0]+1,location[1]),(location[0]-1,location[1]),(location[0],location[1]+1),(location[0],location[1]-1)]
+    #     if any([self.get_tile_at(coord).reward > 0 for coord in neighbors]):
+    #         return True # returns true if the positive reward is in the neighborhood
+    #     else:
+    #         for neighbor in neighbors:
+    #             if not self.get_tile_at(neighbor).obstructed:
+    #                 self.prove_solvable(location=neighbor,visited=visited)
+    #         return False # returns false if running out of neighbor options
 
 class Agent:
-    actions = range(4)
-
     def __init__(self, world):
-        self.location = Gridworld.agent_starting_location
-        self.Q_table = np.ones((world.size, world.size, len(Agent.actions))) #only zeros not supported by Agent's action choice yet
-        #self.tiles_visited = []
+        self.location = Gridworld.agent_location
+        self.Q_table = np.ones((world.size, world.size, len(Agent.actions)))
         self.world = world
+        self.reward = 0
+        self.terminal = False
 
-    def action(self):
+    def action(self, epsilon=0.1):
+        if not self.terminal:
+            #for now, acion is chosen probabilistically based on Q-values
+            current_tile = self.world.get_tile_at(self.location)
+            options = np.multiply(np.array(current_tile.options,dtype='int32'),np.array([1,2,3,4]))
+            options = options[options != 0] - 1 #minus 1 for converting to index
+            action = None
 
-        #for now, acion is chosen probabilistically based on Q-values
-        current_tile = self.world.get_tile_at(self.location)
-        rnd_weights = self.Q_table[self.location[0], self.location[1], :] * current_tile.options
-        rnd_weights /= rnd_weights.sum()
+            if random.random() < epsilon:
+                action = random.choice(options)
+            else:
+                q_values = self.Q_table[self.location[0], self.location[1], :]
+                action = [element for element in np.argsort(q_values)[::-1] if element in options]
 
-        action = random.choices(population = Agent.actions, weights = rnd_weights)[0]
+            state, reward, terminal = self.world.step(action)
 
-        assert current_tile.get_possible(action) == True
+            self.reward = reward
+            self.location = state
+            self.terminal = terminal
 
-        self.location += np.array(current_tile.move(action))
+            return action
 
-def SARSA(agent, world, n):
-    rewards = []
-    for _ in range(n):
-        rewards += [agent.action()] #also implement current location
-#     rewards += [q_val_at_last_action]
+    def reset():
+        self.location = (0,0)
+        self.reward = 0
+        self.cumulative_reward = 0
+        self.terminal = False
+
+def SARSA(agent, world, n=10):
+    if type(n) == int:
+        if not n > 0:
+            raise ValueError('The value of n must not 0 or less.')
+    elif type(n) == str:
+        if not n == 'inf':
+            raise ValueError('The only string option for n is "inf".')
+    else:
+        raise TypeError('The type of n must be either int or string.')
+
+    rewards, locations, actions = [],[(0,0)],[]
+    while not agent.terminal():
+        actions += [agent.action()]
+        rewards += [agent.reward]
+        locations += [agent.location]
+        if n > len(actions):
+            agent.update_q_value(locations[-1],actions[-1],)
+            actions = actions[:-1]
+            rewards = rewards[:-1]
+            locations = locations[:-1]
+
+
     q_val = np.sum([rewards[i]*np.pow(0.98,i) for i in range(len(rewards))])
-    agent.update_q_value()
+    agent.update_q_value(location, action, value)
 
 world = Gridworld()
 #world.prove_solvable()
