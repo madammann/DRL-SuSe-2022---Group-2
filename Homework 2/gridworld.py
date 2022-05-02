d02cdda100f27d83c6246dca1da5234775805889
Marlon Dammann <marlon.dammann@web.de>
2022-05-02 17:04:13 +0200

    implemented SARSA.

--- Homework 2/gridworld.py
+++ Homework 2/gridworld.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 from collections import defaultdict
 import os
+from tqdm import tqdm
 
 class Tile:
     '''The tile class which contains a move method and necessary attributes for implemented functionalities.'''
@@ -13,7 +14,6 @@ class Tile:
 
     def move(self, action):
         '''Calculates a delta for the position of the agent based on choice (0-3 int).'''
-        #Todo: maybe check possiblity of delta movement
         if self.options[action]:
             chance = random.random()
             if self.transition_prob[action][0] >= chance:
@@ -33,27 +33,22 @@ class Gridworld:
         random.seed(seed)
         np.random.seed(seed)
 
-        #generate Gridworld
-
+        '''Generating Gridworld with weights for tile proportions'''
         rnd_weights = np.array([0.5, 0.2, 0.1, 0])
         rnd_weights /= rnd_weights.sum() #sum of weights should be 1
 
-        #0:free space, 1:wall, 2:target, 3:toxic, 4:wind (, 9:agent)
+        #0:free space, 1:wall, 2:target, 3:toxic, 4:wind
         self.grid = np.asarray(random.choices(population = (0,1,3,4), weights = rnd_weights, k = size**2)).reshape(size, size)
         self.grid[self.agent_location] = 0
 
         #set target location
         self.grid[np.random.randint(size/2,size), np.random.randint(size/2,size)] = 2
 
-        #generating wind direction
-        wind = random.randint(0,3)
-
-        '''Creating a transition probability array for wind coming from above, then rotating it to wind direction'''
+        '''Creating a transition probability array for wind coming from above, then rotating it randomly to match a specific direction'''
         wind_prob = np.array([[0.4,0.25,0.1,0.25],[0,0.7,0.3,0],[0,0,1,0],[0,0,0.3,0.7]])
-        if wind > 0:
-            wind_prob = np.rot90(wind_prob, k=wind)
+        wind_prob = np.rot90(wind_prob, k=random.randint(1,4))
 
-        #generating tile class objects
+        '''Generating tile class objects'''
         self.tiles = np.empty((size, size), dtype = object)
         for x in range(self.size):
             for y in range(self.size):
@@ -107,13 +102,6 @@ class Gridworld:
         return self.agent_location, self.get_tile_at(self.agent_location).reward, terminal
 
     def visualize(self, agent):
-
-        #clear screen
-        if os.name == 'posix': #Linux, Mac
-            os.system('clear')
-        else: #Windows
-            os.system('cls')
-
         symbol_map = {0: " ", 1: "\u2588", 2: "t", 3: "\u2622", 9: "a"}
 
         visualization_grid = np.array(self.grid) #deep copy, as to not change the grid array
@@ -123,13 +111,9 @@ class Gridworld:
             for y in range(self.size):
                 print(symbol_map.get(visualization_grid[x, y], "?"), end = "")
             print()
-#             print("\n", "-" * self.size * 2, sep = "")
 
     def get_tile_at(self, coordinates):
         return self.tiles[coordinates[0],coordinates[1]]
-
-    def update_q_value(self,location, action, value):
-        pass
 
     def prove_solvable(self, location=(0,0), visited=[]):
         neighbors = [(location[0]+1,location[1]),(location[0]-1,location[1]),(location[0],location[1]+1),(location[0],location[1]-1)]
@@ -164,7 +148,7 @@ class Agent:
 
     def action(self, epsilon=0.1):
         if not self.terminal:
-            #for now, acion is chosen probabilistically based on Q-values
+            '''Action is chosen probabilistically based on Q-values'''
             current_tile = self.world.get_tile_at(self.world.agent_location)
             options = np.multiply(np.array(current_tile.options,dtype='int32'),np.array([1,2,3,4]))
             options = options[options != 0] - 1 #minus 1 for converting to index
@@ -184,49 +168,115 @@ class Agent:
 
             return action
 
-    def reset():
+    def reset(self):
         self.world.agent_location = (0,0)
         self.reward = 0
-        self.cumulative_reward = 0
         self.terminal = False
-
-def SARSA(agent, world, n=10):
+        
+    def update_q_value(self, location, value):
+        self.Q_table[location] = value
+        
+def SARSA(agent, world, n=10, gamma=0.98, alpha=0.1):
     if type(n) == int:
         if not n > 0:
-            raise ValueError('The value of n must not 0 or less.')
-    elif type(n) == str:
-        if not n == 'inf':
-            raise ValueError('The only string option for n is "inf".')
-    else:
-        raise TypeError('The type of n must be either int or string.')
+            raise ValueError('The value of n must not be 0 or less.')
+    '''Creating empty lists for storing episode data'''
+    states = [(0,0)] #note that states is longer than actions and rewards with the N-th element having no taken action and reward.
+    actions = []
+    rewards = []
 
-    rewards, locations, actions = [],[(0,0)],[]
-    while not agent.terminal():
+    while not agent.terminal:
+        '''Do an action and update the lists'''
         actions += [agent.action()]
+        states += [(agent.location[0],agent.location[1])]
         rewards += [agent.reward]
-        locations += [agent.location]
-        if n > len(actions):
-            agent.update_q_value(locations[-1],actions[-1],)
-            actions = actions[:-1]
-            rewards = rewards[:-1]
-            locations = locations[:-1]
+        
+        '''Update the Q-table as soon as n is exceeded by actions or the terminal state is reached'''
+        if n > len(actions) or agent.terminal:
+            if not agent.terminal:
+                '''Retrieving location for the update and calculating new q value'''
+                location = (states[0][0],states[0][1],actions[0]) #the location for the update which is the first element of states and actions always
+                q_value = agent.Q_table[location] #get current q-value
+                
+                end_location = (states[-1][0],states[-1][1],actions[-1])
+                reward_chain = rewards[0:n-1]+[agent.Q_table[end_location]] #creating the reward chain for n steps with the last step as q-value
+                
+                q_delta = alpha*(np.sum([reward_chain[i]*np.power(gamma,i) for i in range(len(reward_chain))]) - q_value)
+                
+                agent.update_q_value(location, q_value+q_delta) #updating the value in the Q-table
+                
+                '''Deleting the first element since the update has been done'''
+                states = states[1:]
+                actions = actions[1:]
+                rewards = rewards[1:]
+            else:
+                '''Iterate over remaining non-terminal elements and do all updates'''
+                while len(actions) > 0:
+                    '''Retrieving location for the update and calculating new q value'''
+                    location = (states[0][0],states[0][1],actions[0]) #the location for the update which is the first element of states and actions always
+                    q_value = agent.Q_table[location] #get current q-value
+                
+                    q_delta = alpha*(np.sum([rewards[i]*np.power(gamma,i) for i in range(len(rewards))]))
+                
+                    agent.update_q_value(location, q_value+q_delta) #updating the value in the Q-table
+                
+                    '''Deleting the first element since the update has been done'''
+                    states = states[1:]
+                    actions = actions[1:]
+                    rewards = rewards[1:]
 
-
-    q_val = np.sum([rewards[i]*np.pow(0.98,i) for i in range(len(rewards))])
-    agent.update_q_value(location, action, value)
-
-#Create solvable gridworld
+'''Create solvable gridworld'''
+world = None
 for world_seed in range(3, 100):
     world = Gridworld(seed = world_seed)
     if world.prove_solvable()[0] == True:
         break
-
+        
 agent = Agent(world)
 
+'''Visualizing the Gridworld once to show '''
+print('======================')
+print('The Gridworld setup:')
+print('======================')
+print('\n')
 world.visualize(agent)
-#testing agent movement
-for timestep in range(20):
-    print()
-    input("Next action")
-    agent.action()
+
+'''Doing training with SARSA setup'''
+print('\n')
+print('======================')
+print('SARSA:')
+print('======================')
+print('\n')
+
+value = input('Please select a value for n which is a number greater than 0: ')
+while not value.isnumeric():
+    print('Value not allowed, please select another value for n!')
+    print('\n')
+    value = input('Please select a value for n which is a number greater than 0: ')
+        
+for episode in tqdm(range(1000),desc='Doing SARSA Episodes'):
+    SARSA(agent, world, n=int(value))
+    agent.reset()
+    world.reset()
+    
+print('\n')
+print('======================')
+print('Q-Table (x,y,action):')
+print('======================')
+print('\n')
+print(agent.Q_table)
+
+input('Press any key to continue with the solution...')
+
+'''Visualizing the final policy actions'''
+print('\n')
+print('======================')
+print('Gridworld solution:')
+print('======================')
+print('\n')
+
+while not agent.terminal:
+    agent.action(epsilon=0)
     world.visualize(agent)
+    print('\n')
+    
