ef7d9e8306d5d05a615ac15ed9866d22985817bf
Pelin <pkomurluoglu@gmail.com>
2022-06-20 09:55:34 +0200

    experience replay buffer

--- Homework 3/model.py
+++ Homework 3/model.py
@@ -1,23 +1,54 @@
 import tensorflow as tf
+import collections #for deque
+import random
+from tqdm import tqdm 
 
 
 class ExperienceReplayBuffer:
-    def __init__(self):
+    def __init__(self, action_space, size_replaybuffer = 100000, size_batch = 50):
+
         # parameters like length
+        self.size_replaybuffer = size_replaybuffer
+        self.action_space = action_space
+        self.size_batch = size_batch
+        #self.experience = ()
+        self.memory = deque(maxlen=size_replaybuffer) 
         pass
     
-    def append(self, elem):
+    def append(self, state, action, reward, next_state):
         # appending
+        new_experience = (state, action, reward, next_state)
+        self.memory.append(new_experience)
+
         pass
-    
-    def dequeue(self):
+
+    # deque() uses fifo. when the list reaches the maximum length, it will get rid of the first element. 
+
+    def sampling(self):
+        
+        sample_experience = random.sample(self.memory, k=self.size_batch)
+        print(sample_experience)
+
+        return sample_experience
+
+    def fill_buffer(self):
+        
+        for i in tqdm(range(self.size_replaybuffer), desc = "Buffer Progress"):
+          buff = []
+          buff.append(i)
+        pass
+        
+    #def dequeue(self):
         # first out
-        pass
+                   
+
+     #   pass
     
-    def pop(self):
-        pass # return some elements and call dequeue
+    #def pop(self):
+     #   pass # return some elements and call dequeue
     
     # add functions for getter returning as tfds
+
     
 class LunarLanderModel(tf.keras.Model):
     def __init__(self):
