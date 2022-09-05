d1e475d3b686f1b92d15edcde439b508e5e69a42
Marlon Dammann <64379599+madammann@users.noreply.github.com>
2022-09-05 19:39:17 +0200

    added non-base agents

--- final/agents.py
+++ final/agents.py
@@ -1,12 +1,170 @@
+import numpy as np
+import tensorflow as tf
+
+from environment import ConnectFourEnv
+
+from minimax import MinimaxNode, Minimax
+from model import ConnectFourModel
+
+from copy import deepcopy
 
 class AgentBase:
-    pass
+    '''
+    ADD
+    '''
+    
+    def __init__(self):
+        self.winrate = np.nan()
+        self.games = 0
 
 class MinimaxAgent(AgentBase):
-    pass
+    '''
+    ADD
+    '''
+    
+    def policy(self, env):
+        '''
+        ADD
+        '''
+        
+        self.depthmax = self.select_appropriate_depthmax(env)
+        tree = Minimax(env, depthmax=self.depthmax)
+        
+        policy = tree(env.turn)
+        
+        return policy
+    
+    def select_move(self, env):
+        '''
+        ADD
+        '''
+        
+        policy = self.policy(env)
+        
+        action = np.argmax(policy)
+        
+        return action
+    
+    def select_appropriate_depthmax(self, env):
+        '''
+        ADD
+        '''
+        
+        branching_factor = len(env.action_range)
+        node_max = 10e8
+        
+        nodes = branching_factor
+        depth = 1
+        while True:
+            nodes += nodes*branching_factor
+            
+            if nodes <= node_max:
+                depth += 1
+                
+            else:
+                return depth
 
 class RandomAgent(AgentBase):
-    pass
+    '''
+    ADD
+    '''
+    
+    def policy(self, env):
+        '''
+        ADD
+        '''
+        
+        policy = np.random.uniform(low=0, high=1, shape=(len(env.action_range)), dtype='float32')
+        
+        return policy
+    
+    def select_move(self, env):
+        '''
+        ADD
+        '''
+        
+        policy = self.policy(env)
+        
+        return np.argmax(policy)
+
+class AvoidNextLossAgent(AgentBase):
+    '''
+    ADD
+    '''
+    
+    def policy(self, env):
+        '''
+        ADD
+        '''
+
+        policy = np.full((10), 1, dtype='float32')
+    
+        args = [deepcopy(env) for _ in range(len(env.action_range))]
+        
+        return policy
+    
+    def select_move(self, env):
+        '''
+        ADD
+        '''
+        
+        policy = self.policy(env)
+        choice = np.argmax(policy)
+        
+        return choice
+
+    def create_all_child_envs(self, env):
+        '''
+        ADD
+        '''
+        
+        child_envs = []
+        
+        for action in range(len(env.action_range)):
+            child_env = deepcopy(env)
+            child_env.step(action)
+            child_env += [child_env]
+        
+        return child_envs
 
 class ModelAgent(AgentBase):
-    pass
+    '''
+    ADD
+    '''
+    
+    #TBD Implenet a __init__ overwrite of parent class which allows for specific model loading
+    def __init__(self, model_name : str):
+        '''
+        ADD
+        '''
+        
+        super(ModelAgent, self).__init__()
+        
+        self.model = None #load a specific model here
+        self.input_size = None #add input sized for models by name here
+        
+    def policy(self, env):
+        '''
+        ADD
+        '''
+        
+        obs = env.grid2obs()
+        input_size = tuple(obs.shape) #may not work change after testing
+        
+        if input_size != self.input_size:
+            raise ValueError('Model does not support input shape of passed environment.')
+        
+        policy = self.model(obs).numpy()
+        
+        return policy
+    
+    def select_move(self, env):
+        '''
+        ADD
+        '''
+        
+        policy = self.policy(env)
+        
+        action = np.argmax(policy)
+        
+        return action
--- final/evaluation_func.py
+++ final/evaluation_func.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 from environment import ConnectFourEnv
 
@@ -8,7 +8,45 @@ class EvaluationFunction:
     '''
     
     def __init__(self, params):
+        '''
+        ADD
+        '''
+        
+        self.weights = np.ones((3),dtype='float32')
+#         self.meta_weights = np.ones((3),dtype='float32') #may be implemented later
+        
+    def __call__(self, env, player : bool):
+        '''
+        ADD
+        '''
+        
+        obs = env.grid2obs()
+        
+        values = np.zeros((3),dtype='float32')
+        
+        values[0] = self.get_possible_wins_val(obs, player) * self.weights[0]
+        values[1] = self.get_options_left_val(obs, player) * self.weights[1]
+        values[2] = self.get_open_chains_val(obs, player) * self.weights[2]
+        
+        return np.sum(values)
+        
+    def get_possible_wins_val(self, obs, player : bool):
+        '''
+        ADD
+        '''
+        
         pass
     
-    def __call__(self, env):
+    def get_options_left_val(self, obs, player : bool):
+        '''
+        ADD
+        '''
+        
+        pass
+    
+    def get_open_chains_val(self, obs, player : bool):
+        '''
+        ADD
+        '''
+        
         pass
